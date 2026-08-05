e1a93ffae8090b5c62823ba4eb99779e0722db6d
t <t@t>
2026-08-05 17:25:05 +0200

    init: base

--- packages/web/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { t } from './i18n'
-import './style.css'
-
-const app = createApp(App)
-app.config.globalProperties.$t = t
-app.mount('#app')
--- prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-export default {
-  semi: false,
-  singleQuote: true,
-  printWidth: 100,
-  plugins: ['@ianvs/prettier-plugin-sort-imports'],
-  importOrder: ['^node:', '^@?\\w', '^\\.'],
-  importOrderTypeScriptVersion: '5.0.0',
-}
